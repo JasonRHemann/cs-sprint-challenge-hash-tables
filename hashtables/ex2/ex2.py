5bdc10a781f1f70bb1c03511d20ae223cfe52b2a
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-07-10 11:03:46 -0500

    1 down

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,18 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    trip = {}
+    route = []
+
+    for ticket in tickets:
+        trip[ticket.source] = ticket.destination
+
+    next_trip = trip['NONE']
+
+    while next_trip != "NONE":
+        route.append(next_trip)
+        next_trip = trip[next_trip]
+
+    route.append("NONE")
 
     return route
