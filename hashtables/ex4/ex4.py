d8ae6cbed189fd03b31ad71f9c1dd44aada6db56
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-07-10 11:52:36 -0500

    2 down

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,12 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    numbers_dict = {}
+    result = []
+
+    for num in a:
+        if numbers_dict.get(abs(num)):
+            result.append(abs(num))
+        else:
+            numbers_dict[abs(num)] = num
 
     return result
 
